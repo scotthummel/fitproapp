9dda75ab88bb8327b505edb9eba1b07f0a67af88
Scott Hummel <scotthummel@icloud.com>
2017-10-14 16:09:33 -0700

    Added email address to title bar

--- src/components/navbar/navbar.ts
+++ src/components/navbar/navbar.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import {FirebaseService} from "../../providers/firebase-service";
+import {AngularFireAuth} from "angularfire2/auth";
+import {AngularFireDatabase} from "angularfire2/database";
+import {App} from "ionic-angular";
 
 @Component({
   selector: 'navbar',
   templateUrl: 'navbar.html'
 })
-export class Navbar {
-  constructor() {
+export class Navbar extends FirebaseService{
+  constructor(public afAuth: AngularFireAuth, public afd: AngularFireDatabase, public app: App) {
+    super(afAuth, afd, app);
   }
-
 }
